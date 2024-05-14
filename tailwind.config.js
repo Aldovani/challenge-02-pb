7d341d94b922758a553d64d0937ff0e59b0b1bdb
Beatriz Zanchin <betrizmesias@gmail.com>
2024-05-14 15:13:02 -0300

    feat(register): setup inicial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html", "./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FCFCFC",
+        "athena-grey": "#F8F9FB",
+        wisper: "#F0F1EE",
+        primary: {
+          accent: "#DDDEA0",
+          avacado: "#8A9B6E",
+          "lunar-green": "#354733",
+        },
+        dark: {
+          IBBNB: "#1E1E24",
+          gray: "#AFB2AA",
+        },
+        secondary: {
+          Teak: "#B99274",
+          secondary: "#808276",
+        },
+      },
+    },
   },
   plugins: [],
 };
