065f3537a825911b58834f3b164184030e3292f8
Aldovani <aldovanihcosta@gmail.com>
2024-05-14 22:15:32 -0300

    feat: create plants service

---
+++ src/types/Plants.ts
@@ -0,0 +1,12 @@
+export interface Plants {
+  id: number;
+  name: string;
+  subtitle: string;
+  label: string[];
+  price: string;
+  isInSale: boolean;
+  discountPercentage: number;
+  features: string;
+  description: string;
+  imgUrl: string;
+}
