8c8f355f382ac84048ff069a5228ff2f38096921
Aldovani <aldovanihcosta@gmail.com>
2024-05-15 18:11:32 -0300

    chore: change  path ths header component

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,30 @@
+import logo from "../../assets/logo.svg";
+import user from "../../assets/user.png";
+import { ActiveLink } from "./ActiveLink";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center p-2 pl-20 pr-20 bg-athena-grey">
+      <img src={logo} alt="Logo da Plant Pace" />
+      <nav className="flex flex-row flex-wrap">
+        <ul className="flex flex-row gap-6">
+          <li className="">
+            <ActiveLink to="/">Home</ActiveLink>
+          </li>
+          <li className="text-primary-lunar-green">
+            <ActiveLink to="/register">Register</ActiveLink>
+          </li>
+          <li className="">
+            <ActiveLink to="/products">Products</ActiveLink>
+          </li>
+          <li className="text-primary-lunar-green">
+            <ActiveLink to="/about">About us</ActiveLink>
+          </li>
+        </ul>
+      </nav>
+      <img src={user} alt="Exemplo" />
+    </header>
+  );
+};
+
+export default Header;
