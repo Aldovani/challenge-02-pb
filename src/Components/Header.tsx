211fd9055b5e4f77abc037ace0b112ace1ba0076
TalitaFavore <talitafavore@gmail.com>
2024-05-15 13:47:59 -0300

    feat: structuring header component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,21 @@
-export function Header(){
-  return  (
-    <h1>header</h1>
-  )}
+import logo from '../assets/logo.svg';
+import user from '../assets/user.png';
+
+const Header = () => {
+  return (
+    <div>
+        <img src={logo} alt="Exemplo" />
+        <div>
+        <ul>
+        <li>Home </li>
+        <li>Register </li>
+        <li>Products </li>
+        <li>About us </li>
+      </ul>
+        </div>
+      <img src={user} alt="Exemplo" />
+    </div>
+  )
+}
+
+export default Header
