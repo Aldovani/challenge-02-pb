d101dcd39e3ea0fc30a52c9230a7671cbfdc5f51
itsmecamila <camilamarq.depaula@gmail.com>
2024-05-15 16:56:01 -0300

    feat: create PlantCard component

---
+++ src/components/PlantCard.tsx
@@ -0,0 +1,15 @@
+
+export default function PlantCard(){
+    return <>
+
+    <div>
+        <img src="/plant1.png" alt="First Card Plant" />
+        <div>
+            <h2>Echinocereus Cactus</h2>
+            <p>$15.00</p>
+            <div>indoor</div>
+        </div>
+    </div>
+    </>
+
+}
