3bf8b053c9357d38933ad9810c523c4c88c206d8
TalitaFavore <talitafavore@gmail.com>
2024-05-16 14:58:07 -0300

    style: styling Steps component responsive design

--- src/Components/Steps.tsx
+++ src/Components/Steps.tsx
@@ -4,9 +4,9 @@ import img3 from "../assets/img3.png";
 
 const Steps = () => {
   return (
-    <section className="m-20 mb-30 bg-white">
+    <section className="flex flex-col m-auto mt-20 md:m-20 md:mb-30 bg-white">
       <div className="flex flex-col m-auto text-center w-7/12">
-        <h1 className="text-xl font-bold font-eb-garamond text-primary-lunar-green">
+        <h1 className="text-xl2 md:text-xl font-bold font-eb-garamond text-primary-lunar-green">
             Steps To Take Care Of Your <span className="text-primary-avacado">Plants</span></h1>
         <p className="text-xs font-raleway mb-20 text-dark-gray">
           By following these three steps - proper watering, appropriate
@@ -15,16 +15,16 @@ const Steps = () => {
         </p>
       </div>
 
-      <div className="flex flex-row w-full justify-between">
-        <div className="flex w-96">
+      <div className="flex flex-col gap-6 lg:flex-row w-full justify-between">
+        <div className="w-72 flex m-auto md:mt-0 md:w-96">
         <img src={img1} alt="Imagem de um ambiente interno com muitas folhagens" />
         </div>
         
-        <div className="flex w-96">
+        <div className="w-72 flex m-auto md:mt-0 md:w-96">
         <img src={img2} alt="Imagem de um ambiente externo com muitas folhagens" />
         </div>
        
-        <div className=" flex flex-col w-96">
+        <div className="w-72 flex flex-col m-auto md:mt-0 md:w-96">
           <img src={img3} alt="Imagem de uma janela redonda com um bonsai" />
           <p className="text-sm mt-6 leading-6 text-secondary-secondary">
             Our website offers a wide array of stunning plants, ranging from
