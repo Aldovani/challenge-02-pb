27917b25ef1ccc2ca3a59faf69163a8121016582
Everton Ceciliano <evertoncecilianosouza@gmail.com>
2024-05-15 21:06:27 -0300

    Merge feat/everton/buttons into dev

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 
 
 function App() {
-<<<<<<<<< Temporary merge branch 1
-  return 
-  <>
-  <BrowserRouter>
-    <Router />
-  </BrowserRouter>
-  </>;
-=========
   return (
     <>
       <BrowserRouter>
         <Router />
       </BrowserRouter>
     </>
-  );
->>>>>>>>> Temporary merge branch 2
-}
-
+  );}
+  
 export default App;
