a8a5169a9e83018cc313a925daa0a26df72b37c9
TalitaFavore <talitafavore@gmail.com>
2024-05-17 15:42:32 -0300

    feat: add activeLink to full header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -2,18 +2,32 @@ import { UserButton } from "@clerk/clerk-react";
 import logo from "../../assets/logo.svg";
 import { ActiveLink } from "./ActiveLink";
 import { useNavigate } from "react-router-dom";
-
+import { useState } from 'react';
+ 
 const Header = () => {
-
+  const [isOpen, setIsOpen] = useState(false);
+ 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+ 
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/home");
   };
+ 
   return (
     <header className="flex justify-between items-center p-2 pl-20 pr-20 bg-athena-grey">
-      <img onClick={handleClick} className="cursor-pointer" src={logo}  alt="Logo da Plant Pace" />
-      <nav className="flex flex-row flex-wrap">
-        <ul className="flex flex-row gap-6">
+            <img onClick={handleClick} className="cursor-pointer" src={logo}  alt="Logo da Plant Pace" />
+            <div className="flex-1 flex justify-end lg:hidden">
+          <button onClick={toggleMenu}>
+            <svg className="w-6 h-6" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+        </div>
+        <nav>
+        <ul className="hidden lg:flex lg:flex-row lg:flex-wrap lg:gap-6">
           <li>
             <ActiveLink to="/">Home</ActiveLink>
           </li>
@@ -27,10 +41,40 @@ const Header = () => {
             <ActiveLink to="/about">About us</ActiveLink>
           </li>
         </ul>
-      </nav>
-      <UserButton />
+        </nav>
+        <div className="hidden lg:block">
+        <UserButton  />
+        </div>
+       
+ 
+      <div className={`fixed top-0 right-0 h-full z-50 bg-athena-grey transition-transform transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} w-3/5 lg:hidden`}>
+        <button onClick={toggleMenu} className="absolute top-4 right-4">
+          <svg className="w-6 h-6 text-black" stroke="black" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
+          </svg>
+        </button>
+       
+        <nav >
+        <ul className="flex flex-col mt-16 gap-4 ml-8 text-lg">
+          <li>
+            <ActiveLink to="/">Home</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to="/register">Register</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to="/products">Products</ActiveLink>
+          </li>
+          <li>
+            <ActiveLink to="/about">About us</ActiveLink>
+          </li>
+          <UserButton/>
+        </ul>
+        
+        </nav>
+      </div>
     </header>
   );
 };
-
+ 
 export default Header;
