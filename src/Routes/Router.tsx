3c85dd82f97385b7e1150654243e85d4320a6c1d
Aldovani <aldovanihcosta@gmail.com>
2024-05-17 15:19:48 -0300

    feat: create sign page

--- src/Routes/Router.tsx
+++ src/Routes/Router.tsx
@@ -5,19 +5,19 @@ import { PlantsRegistration } from "../pages/PlantsRegistration";
 import { ProductDetails } from "../pages/ProductsDetails";
 import { ProductList } from "../pages/ProductList";
 import { DefaultLayout } from "../layouts/DefaultLayout";
-import { SignIn } from "@clerk/clerk-react";
+import { SignIn } from "../pages/SignIn";
 
 export function Router() {
   return (
     <Routes>
-    <Route element={<DefaultLayout/>}>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<AboutUs />} />
-      <Route path="/plants-registration" element={<PlantsRegistration />} />
-      <Route path="/product-details" element={<ProductDetails />} />
-      <Route path="/product-list" element={<ProductList />} />
-    </Route>
-      <Route path="/sing-in" element={<SignIn/>} />
-  </Routes>
+      <Route element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/plants-registration" element={<PlantsRegistration />} />
+        <Route path="/product-details" element={<ProductDetails />} />
+        <Route path="/product-list" element={<ProductList />} />
+      </Route>
+      <Route path="/sing-in" element={<SignIn />} />
+    </Routes>
   );
 }
