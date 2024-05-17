60a63ac7683846d6b4937d02e064799c7603ccad
Aldovani <aldovanihcosta@gmail.com>
2024-05-17 00:04:34 -0300

    feat: team component

---
+++ src/Components/Team/index.tsx
@@ -0,0 +1,25 @@
+import profiles from "../../constants/profiles.json";
+import { TeamItem } from "./item";
+
+export function Team() {
+  return (
+    <section className="bg-wisper sm:px-20 px-5  py-20">
+      <div className="container mx-auto">
+        <h2 className="sm:text-8xl text-4xl font-eb-garamond font-bold text-primary-lunar-green">
+          Team
+        </h2>
+
+        <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-6 mt-10">
+          {profiles.map((profile) => (
+            <TeamItem
+              imageUrl={profile.imageUrl}
+              name={profile.name}
+              path={profile.slug}
+              key={profile.slug}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Components/Team/item.tsx
@@ -0,0 +1,27 @@
+import { NavLink } from "react-router-dom";
+
+type TeamItemProps = {
+  path: string;
+  imageUrl: string;
+  name: string;
+};
+
+export function TeamItem({ imageUrl, path, name }: TeamItemProps) {
+  const firstName = name.split(" ")[0];
+  return (
+    <NavLink to={`/about/${path}`} className="overflow-hidden  relative h-full">
+      <div
+        className={`hover:scale-[1.2] transition-all bg-[url(${imageUrl})]  bg-cover bg-center h-[550px]`}
+      >
+        <div className="flex flex-col items-center gap-1 absolute bottom-20 left-1/2 -translate-x-1/2">
+          <h4 className="font-eb-garamond font-bold text-[2.5rem] text-white">
+            {firstName}
+          </h4>
+          <h6 className="text-dark-gray text-[1.5rem] font-eb-garamond">
+            FullStack
+          </h6>
+        </div>
+      </div>
+    </NavLink>
+  );
+}
