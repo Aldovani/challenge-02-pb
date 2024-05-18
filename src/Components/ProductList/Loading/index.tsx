17a4869788dcc7377a3da2ce4eeb22b335295b8d
Aldovani <aldovanihcosta@gmail.com>
2024-05-18 16:36:39 -0300

    feat: create ProductLoadList component

---
+++ src/Components/ProductList/Loading/index.tsx
@@ -0,0 +1,13 @@
+import { ProductLoadItem } from "./item";
+
+export function ProductLoadList() {
+  const numberOfElements = new Array(20).fill(() => "");
+
+  return (
+    <>
+      {numberOfElements.map(() => (
+        <ProductLoadItem />
+      ))}
+    </>
+  );
+}
---
+++ src/Components/ProductList/Loading/item.tsx
@@ -0,0 +1,13 @@
+export function ProductLoadItem() {
+  return (
+    <div className="animate-pulse max-w-96 w-full bg-white  shadow-card-plant">
+      <div className="animate-pulse object-cover w-full bg-wisper h-96 " />
+      <main className=" pt-4 pb-8 px-8">
+        <div className="animate-pulse bg-wisper w-1/2 h-8 "></div>
+
+        <div className="animate-pulse bg-wisper w-1/3 mt-3 h-4 "></div>
+        <div className="animate-pulse  bg-wisper w-1/4 mt-6 px-3 py-1.5 rounded-full"></div>
+      </main>
+    </div>
+  );
+}
