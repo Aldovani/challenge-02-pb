e253c56f5c6b1135b03ee272ed9eda61da41c899
Aldovani <aldovanihcosta@gmail.com>
2024-05-19 00:28:36 -0300

    fix: add Provider container

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { ClerkProvider } from '@clerk/clerk-react'
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-}
+import { Providers } from "./providers/index.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+    <Providers>
       <App />
-    </ClerkProvider>
+    </Providers>
   </React.StrictMode>,
 );
