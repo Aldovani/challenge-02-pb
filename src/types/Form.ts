6c48c21223d04747d311a144146068fa948c5cf2
Biazanchin <165194563+Biazanchin@users.noreply.github.com>
2024-05-19 17:39:23 -0300

    test: add request

---
+++ src/types/Form.ts
@@ -0,0 +1,13 @@
+export type FormField =
+  | "plantName"
+  | "plantSubtitle"
+  | "plantType"
+  | "price"
+  | "discount"
+  | "label"
+  | "features"
+  | "description";
+
+export type FormErrors = {
+  [key in FormField]?: string;
+};
