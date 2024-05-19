c3b6c3c423b92c48e731b812920800b10a0b723b
Aldovani <aldovanihcosta@gmail.com>
2024-05-19 00:36:19 -0300

    fix: add steps to home and delete duplicated component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 import HeroHome from "../Components/HeroHome";
+import Steps from "../Components/Steps/";
 
 export function Home() {
-  return <HeroHome />;
+  return (
+    <>
+      <HeroHome />
+      <Steps />
+    </>
+  );
 }
