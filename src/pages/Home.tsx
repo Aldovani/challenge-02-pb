66629a0c8d45b34488aed45eafd7660dd1910f28
Aldovani <aldovanihcosta@gmail.com>
2024-05-19 16:54:32 -0300

    feat: add Slid to home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroHome from "../Components/HeroHome";
 import Steps from "../Components/Steps/";
+import Slide from "../Components/slide/Index";
 
 export function Home() {
   return (
     <>
       <HeroHome />
       <Steps />
+      <Slide />
     </>
   );
 }
