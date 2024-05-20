90fc667737bf01c940e8952c8ad729c3dfb0d9c5
Aldovani <aldovanihcosta@gmail.com>
2024-05-20 13:59:14 -0300

    chore: remove blank spaces

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import { Router } from "./Routes/Router";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
-
- 
- 
   return (
-    <> 
+    <>
       <BrowserRouter>
-        <Router/>
+        <Router />
       </BrowserRouter>
     </>
   );
