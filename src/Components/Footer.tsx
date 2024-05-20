53389128f1c9f91d993e61e2ed5d293c8d6fe989
Aldovani <aldovanihcosta@gmail.com>
2024-05-20 01:25:38 -0300

    styles:  add container

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,13 +1,13 @@
-import logo from "../assets/logo2.svg";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo2.svg";
 
 const Footer = () => {
   return (
     <footer
       className={`font-raleway text-xs bg-primary-lunar-green bg-footer-image text-white pb-14`}
     >
-      <div className="container mx-auto" >
-        <div className="flex flex-col md:flex-row justify-center items-center md:justify-between p-2 pl-20 pr-20">
+      <div className="container mx-auto">
+        <div className="flex flex-col md:flex-row  justify-center items-center md:justify-between p-2 pl-20 pr-20">
           <div className="mt-14 mb-20 md:mt-32">
             <h1 className="font-eb-garamond font-bold text-xl2 md:text-xl mb-10">
               Stay Fresh
@@ -18,19 +18,37 @@ const Footer = () => {
           <div className="flex gap-10 md:mt-40">
             <div>
               <ul>
-                <li className="cursor-pointer font-bold text-lg pb-4">Links</li>
-                <li className="cursor-pointer font-thin pb-2">About us</li>
-                <li className="cursor-pointer font-thin pb-2">Products</li>
-                <li className="cursor-pointer font-thin">Blogs</li>
+                <li className="font-bold text-lg pb-4">Links</li>
+                <li className="font-thin pb-2 hover:text-primary-accent">
+                  {" "}
+                  <Link to="/about">About us</Link>
+                </li>
+                <li className="font-thin pb-2 hover:text-primary-accent">
+                  {" "}
+                  <Link to="*">Products</Link>
+                </li>
+                <li className="font-thin hover:text-primary-accent">
+                  {" "}
+                  <Link to="*">Blogs</Link>
+                </li>
               </ul>
             </div>
 
             <div>
               <ul>
-                <li className="cursor-pointer font-bold text-lg pb-4">Comunity</li>
-                <li className="cursor-pointer font-thin pb-2">About us</li>
-                <li className="cursor-pointer font-thin pb-2">Products</li>
-                <li className="cursor-pointer font-thin">Blogs</li>
+                <li className="font-bold text-lg pb-4">Comunity</li>
+                <li className="font-thin pb-2 hover:text-primary-accent">
+                  {" "}
+                  <Link to="/about">About us</Link>
+                </li>
+                <li className="font-thin pb-2 hover:text-primary-accent">
+                  {" "}
+                  <Link to="*">Products</Link>
+                </li>
+                <li className="font-thin hover:text-primary-accent">
+                  {" "}
+                  <Link to="*">Blogs</Link>
+                </li>
               </ul>
             </div>
           </div>
@@ -42,39 +60,19 @@ const Footer = () => {
 
         <div className="flex justify-between p-4 pl-10 pr-10 md:pl-20 md:pr-20 ">
           <div>
-            <ul>
-              <li className="font-bold text-lg pb-4">Links</li>
-              <li className="font-thin pb-2 hover:text-primary-accent"> <Link to="/about">About us</Link></li>
-              <li className="font-thin pb-2 hover:text-primary-accent"> <Link to="*">Products</Link></li>
-              <li className="font-thin hover:text-primary-accent"> <Link to="*">Blogs</Link></li>
-            </ul>
+            <Link to="/">
+              <img
+                src={logo}
+                alt="Logo da Plant Pace"
+                className="cursor-pointer"
+              />
+            </Link>
           </div>
 
-          <div >
-            <ul>
-              <li className="font-bold text-lg pb-4">Comunity</li>
-              <li className="font-thin pb-2 hover:text-primary-accent"> <Link to="/about">About us</Link></li>
-              <li className="font-thin pb-2 hover:text-primary-accent"> <Link to="*">Products</Link></li>
-              <li className="font-thin hover:text-primary-accent"> <Link to="*">Blogs</Link></li>
-            </ul>
+          <div className="font-thin flex flex-col justify-center items-center w-30 ">
+            <p>Compassinhos ®. All rights</p>
+            <p>reserved.</p>
           </div>
-        </div>
-      </div>
-
-      <div className="mt-20 ml-10 mr-10 md:mt-24 md:ml-20 md:mr-20">
-        <hr className="border-primary-accent" />
-      </div>
-
-      <div className="flex justify-between p-4 pl-10 pr-10 md:pl-20 md:pr-20 ">
-        <div>
-        <Link to="/">
-            <img src={logo} alt="Logo da Plant Pace" className="cursor-pointer"/>
-          </Link>
-        </div>
-
-        <div className="font-thin flex flex-col justify-center items-center w-30 ">
-          <p>Compassinhos ®. All rights</p>
-          <p>reserved.</p>
         </div>
       </div>
     </footer>
