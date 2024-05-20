8eb435440c8aac0e89e7da732874a021995a37eb
Aldovani <aldovanihcosta@gmail.com>
2024-05-19 21:10:13 -0300

    chore: add path backgroundImage

--- src/Components/Team/item.tsx
+++ src/Components/Team/item.tsx
@@ -9,12 +9,14 @@ type TeamItemProps = {
 
 export function TeamItem({ imageUrl, path, name, role }: TeamItemProps) {
   const firstName = name.split(" ")[0];
+
   return (
     <NavLink to={`/about/${path}`} className="overflow-hidden  relative h-full">
       <div
-        className={`hover:scale-[1.2] transition-all bg-[url(${imageUrl})]  bg-cover bg-center h-[550px]`}
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className={`hover:scale-[1.2] bg-cover bg-center relative after:w-full after:h-full after:absolute after:block after:bg-gradient-to-t after:from-black/50  after:inset-0 after:pointer-events-none transition-all  h-[550px]`}
       >
-        <div className="flex flex-col items-center gap-1 absolute bottom-20 left-1/2 -translate-x-1/2">
+        <div className="flex z-20 flex-col items-center gap-1 absolute bottom-20 left-1/2 -translate-x-1/2">
           <h4 className="font-eb-garamond font-bold text-[2.5rem] text-white">
             {firstName}
           </h4>
